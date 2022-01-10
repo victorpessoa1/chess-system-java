dbfef2cebfab23b4fc9f271739b0e14b93d84273
Victorpessoa1 <josevitorppessoa@gmail.com>
2022-01-09 22:33:34 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -54,7 +54,14 @@ public class ChessMatch
 	private void validateSourcePosition(Position position)
 	{
 		if(!board.thereIsAPiece(position))
+		{
 			throw new ChessException("Position not on the Board");
+		}
+		if (!board.piece(position).isThereAnyPossibleMove())
+		{
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
+			
 		
 	}
 	
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -19,4 +19,11 @@ public class King extends ChessPiece
 		return "K";
 	}
 
+	@Override
+	public boolean[][] possibleMoves()
+	{
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+
 }
